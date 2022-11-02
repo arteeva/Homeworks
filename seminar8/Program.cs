31a6177172f2d9034997ce076c91bf226a4a1ffa
Elena <arteeva_2022@list.ru>
2022-11-02 21:46:18 +0300

    задача 54 готова

--- seminar8/Program.cs
+++ seminar8/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
@@ -8,6 +8,69 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] Create2DRandomArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    } 
+    return newArray;
+}
+
+void Show2DArray (int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Decrease(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int column = 1; column < array.GetLength(1); column++)
+            {
+                if(array[i,column] >= array[i,column-1])
+                {
+                    int temp = array[i,column-1];
+                    array[i,column-1] = array[i,column];
+                    array[i,column] = temp;
+                }
+            }
+        }
+    }
+    return array;
+        
+}
+Console.Write("Введите количество строк -> ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальное значение -> ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение -> ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = Create2DRandomArray(m, n, min, max);
+Show2DArray (myArray);
+Console.WriteLine();
+//Decrease(myArray);
+Console.WriteLine("Массив с отсортированными в порядке убывания строками -> ");
+Show2DArray (Decrease(myArray));
+*/
 
 
 
